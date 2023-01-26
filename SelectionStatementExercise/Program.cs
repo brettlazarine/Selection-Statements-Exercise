67f1da6690ac0257050d852f35251426bc7fe1e4
Brett Lazarine <jblazarine@gmail.com>
2023-01-25 21:53:51 -0600

    complete exercise 1 and the switch statement challenge

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Exercise 1
+            var favNumber = 44;
+            Console.Write("Guess my favorite number: ");
+            var userInput = int.Parse(Console.ReadLine());
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("Too low!");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("Too high!");
+            }
+            else
+            {
+                Console.WriteLine("You got it!");
+            }
+            // Completed Extra Switch Statement Practice on Codewars
         }
     }
 }
